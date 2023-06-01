87a192844bd605a3b72c32d1af09fdad298d8f82
ZheChen-Bill <bb890825@gmail.com>
2023-06-02 01:02:31 +0800

    1

--- Source_Separation/main.py
+++ Source_Separation/main.py
@@ -8,10 +8,7 @@ Created on Thu Jun  1 23:58:24 2023
 from demucs.Source_Separation import Source_Separation
 import soundfile as sf
 
-path1 = 'D:\\LAB\\碩一下\\音樂資訊檢索\\final\\MIR-FINAL\\Source_Separation\\separated\\mdx_extra\\idol (mp3cut.net)\\no_vocals.mp3'
-path2 = 'D:\\LAB\\碩一下\\音樂資訊檢索\\final\\MIR-FINAL\\Source_Separation\\separated\\mdx_extra\\idol (mp3cut.net)\\vocals.mp3'
-
-file = 'idol (mp3cut.net).mp3'
+file = '.mp3'
 vocal, no_vocal = Source_Separation(file)
 
 sr = 44100 
